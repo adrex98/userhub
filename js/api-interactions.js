3e18176f0d2cbda8754a9618757ca949a620d03e
Adrian Montesinos <netlinkbolivia@gmail.com>
2023-09-03 16:18:22 -0400

    edit api url

--- js/api-interactions.js
+++ js/api-interactions.js
@@ -1,5 +1,5 @@
   // URL base de tu API
-  const apiUrl = 'http://api-vvw7.onrender.com'; // Cambia la URL según la ubicación de tu API
+  const apiUrl = 'https://api-vvw7.onrender.com/'; // Cambia la URL según la ubicación de tu API
 
   // Función para crear un usuario
   async function crearUsuario(usuario) {
